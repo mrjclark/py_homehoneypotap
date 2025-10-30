302f9868ad58b1946f4ee308349a8ac061ac9d2f
mrjclark <matthew-clark@live.com>
2025-10-29 19:27:52 -0500

    Fix issues in webapp.py and add logging and dev functionality.

--- rpi/py/webapp.py
+++ rpi/py/webapp.py
@@ -1,14 +1,23 @@
-from flask import Flask, Response, abort
+from flask import Flask, Response, abort, request
 import os
 from dotenv import load_dotenv
 import datetime
 
+DEV=True
+
+
 # Variables
 SIEM_USR="siem"
 SIEM_FOLDER=os.path.join("/home",SIEM_USR,"/var/logs")
+LOG_DIR=os.path.join("/var/logs/")
+
+
+if DEV:
+    SIEM_FOLDER="."
+    LOG_DIR="."
 
 # Load environment variables from .env
-load_dotenv("/home/siem/.env")
+load_dotenv(os.path.join(SIEM_FOLDER,".env"))
 
 # Access the token
 API_TOKEN = os.getenv("API_TOKEN")
@@ -20,8 +29,9 @@ def log_request():
     method = request.method
     path = request.path
     ip = request.remote_addr
+    return f"{timestamp} IP:{ip} Method:{method} path:{path}"
     
-
+app = Flask(__name__)
 
 @app.before_request
 def check_token():
@@ -38,13 +48,15 @@ def get_log(log_name):
     path = os.path.join(LOG_DIR, f"{log_name}.log")
     if not os.path.exists(path):
         abort(404)
+    log_msg = log_request()
+    with open("./request_log") as f:
+        f.write(log_msg)
     with open(path) as f:
         return Response(f.read(), mimetype="text/plain")
 
-if __name__ = "main":
-    app.run(
-            host="0.0.0.0" # Accessible only from other devices
-            ,port=5000     # Default
-            ,debug=True    # Auto reload and detailed error pages
+if __name__ == "__main__":
+    app.run(host="0.0.0.0"
+            ,port="5000"
+            ,debug=DEV    # Auto reload and detailed error pages
             )
     
